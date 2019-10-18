0db516becf81155de981ae0bd3fd4c26538e73d3
Ulrik Sandberg <UJSandberg@gmail.com>
2019-10-18 18:20:44 +0200

    Added to deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,13 @@
-ssh root@$PROD_IP 
+ssh root@$PROD_IP << EOF
+
 docker stop $(docker ps -a -q)
+
 docker rm $(docker ps -a -q)
+
 docker run -d -p 80:80 ulriksandberg/portfolio:$CIRCLE_SHA1
+
 exit
+EOF
 
 
 
