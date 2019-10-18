0608179e8f90a2d5609c8ad909fbe73af43355b7
Ulrik Sandberg <UJSandberg@gmail.com>
2019-10-18 18:25:36 +0200

    added ls -a to get an idea of what is going on :D

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,12 @@
 ssh root@$PROD_IP << EOF
 
+docker container ls -a
+
 docker stop $(docker ps -a -q)
 
 docker rm $(docker ps -a -q)
+
+docker container ls -a
 
 docker run -d -p 80:80 ulriksandberg/portfolio:$CIRCLE_SHA1
 
